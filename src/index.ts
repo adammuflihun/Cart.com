4b4369430e8723803a482ed336f16e8be5f0ef72
adammuflihun <96274551+adammuflihun@users.noreply.github.com>
2022-08-19 21:03:17 +0700

    update gsap

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,25 @@
-import { greetUser } from '$utils/greet';
+// import { greetUser } from '$utils/greet';
+import { gsap } from 'gsap';
 
+console.log('oke');
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+  const name = 'John Adam';
+  // greetUser(name);
+});
+
+$('.accordion').click(function () {
+  let g = gsap.utils.selector(this); // or use selector text like ".class"
+
+  $('.accordion .text-content-slide-landing ').hide();
+  $('.accordion .vertical-text ').show();
+  $(this).find('.image-arrow-down').hide();
+  $(this).find('.text-content-slide-landing ').show();
+  $(this).find('.vertical-text ').hide();
+
+  gsap.fromTo(
+    g('.text-content-slide-landing'),
+    { opacity: 0 },
+    { opacity: 1, duration: 0.5, delay: 0.6 }
+  );
 });
