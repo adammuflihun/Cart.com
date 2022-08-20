9250e80a890d23527fef5dd5d9bb6c579a337a98
adammuflihun <96274551+adammuflihun@users.noreply.github.com>
2022-08-20 20:53:24 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -7,17 +7,17 @@ window.Webflow.push(() => {
   // greetUser(name);
 });
 
-$('.accordion').click(function () {
+$('.accordion-header').click(function () {
   let g = gsap.utils.selector(this); // or use selector text like ".class"
 
   $('.accordion .text-content-slide-landing ').hide();
   $('.accordion .vertical-text ').show();
   $('.accordion .image-arrow-down ').show();
-  $(this).find('.image-arrow-down').hide();
-  $(this).find('.text-content-slide-landing ').show();
-  $(this).find('.vertical-text ').hide();
+  $(this).siblings('.image-arrow-down').hide();
+  $(this).siblings('.text-content-slide-landing ').show();
+  $(this).siblings('.vertical-text ').hide();
   gsap.fromTo(
-    g('.text-content-slide-landing'),
+    '.text-content-slide-landing',
     { opacity: 0 },
     { opacity: 1, duration: 0.5, delay: 0.6 }
   );
